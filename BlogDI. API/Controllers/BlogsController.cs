349a2b27bc74f2615a862820fcc77ed4d5cfc8e1
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2023-11-27 23:06:54 +0530

    Add project files.

---
+++ BlogDI. API/Controllers/BlogsController.cs
@@ -0,0 +1,26 @@
+﻿using BlogDI._API.Data;
+using BlogDI._API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlogDI._API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        // data ganne mona location ekend kiyann oni 
+
+        BlogRepository  repository = new BlogRepository();
+
+        // GET: api/<BlogsController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Blog>> Get()
+        {
+            var blogs = repository.getAllBlogs();
+            return Ok(blogs);
+        }
+
+    }
+}
---
+++ BlogDI. API/Data/BlogRepository.cs
@@ -0,0 +1,27 @@
+﻿using BlogDI._API.Model;
+
+namespace BlogDI._API.Data
+{
+    public class BlogRepository
+    {
+        // create a blog list ( this is our data set now => because stil we donot have data base )
+
+        public static List<Blog> blogs = new List<Blog>() 
+        {
+            new Blog(){ Id = 1 ,Content ="this is five episodes", 
+                Title =" time newroman  " },
+
+            new Blog(){ Id = 2 ,Content ="nine episodes",
+                Title =" the dragon " },
+
+            new Blog(){ Id = 3 ,Content ="seven episodes",
+                Title =" lover the best " },
+        };
+
+
+        public List<Blog> getAllBlogs() 
+        { 
+            return blogs;
+        }
+    }
+}
